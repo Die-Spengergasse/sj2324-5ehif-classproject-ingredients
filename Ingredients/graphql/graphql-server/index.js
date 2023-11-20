581f0218fbc4a880c33a9d4fdd5e5b17937e4f66
Somya Rathee <somyarathee07@gmail.com>
2023-11-20 09:24:04 +0100

    changed index.js

--- Ingredients/graphql/graphql-server/index.js
+++ Ingredients/graphql/graphql-server/index.js
@@ -1,20 +1,25 @@
-import { ApolloServer, gql } from 'apollo-server';
+import { gql } from 'apollo-server';
 import axios from 'axios';
 
-// Define your GraphQL schema using gql
 const typeDefs = gql`
   type Recipe {
-    # Define the fields you want to retrieve from the Spoonacular API response
+    id: ID!
     title: String
-    // Add more fields as needed
+    image: String
+    ingredients: [String]
+    instructions: String
+    # Add more fields as needed
   }
 
   type Query {
-    searchRecipes(query: String!, cuisine: String!, diet: String!): [Recipe]
+    searchRecipes(
+      query: String!
+      cuisine: String!
+      diet: String!
+    ): [Recipe]
   }
 `;
 
-// Define your resolver function
 const resolvers = {
   Query: {
     searchRecipes: async (_, { query, cuisine, diet }) => {
@@ -28,16 +33,19 @@ const resolvers = {
           // Add more parameters as needed
         },
         headers: {
-          'X-RapidAPI-Key': '',
+          'X-RapidAPI-Key': '3e20366858msh83cd201175488e0p12db60jsn68c137092f39',
           'X-RapidAPI-Host': 'spoonacular-recipe-food-nutrition-v1.p.rapidapi.com',
         },
       };
 
       try {
         const response = await axios.request(options);
-        // Map the Spoonacular API response to the shape defined in your GraphQL schema
         const recipes = response.data.results.map((result) => ({
+          id: result.id,
           title: result.title,
+          image: result.image,
+          ingredients: result.extendedIngredients.map((ingredient) => ingredient.name),
+          instructions: result.instructions,
           // Map other fields as needed
         }));
 
@@ -50,13 +58,4 @@ const resolvers = {
   },
 };
 
-// Create an instance of ApolloServer
-const server = new ApolloServer({
-  typeDefs,
-  resolvers,
-});
-
-// Start the server
-server.listen().then(({ url }) => {
-  console.log(`Server ready at ${url}`);
-});
+export { typeDefs, resolvers };
