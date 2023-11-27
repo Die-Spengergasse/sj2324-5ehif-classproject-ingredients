31dcc8f8485cd0fa220dce4374e0e2ae97917b9c
Alexander ZIVNY-HARTIG <ziv20434@spengergasse.at>
2023-11-27 20:41:41 +0100

    feat: add swagger ui
    Fix invalid configuration
    Change project type to Sdk.Web

--- Ingredients/Model/Category.cs
+++ Ingredients/Model/Category.cs
@@ -4,8 +4,8 @@ public class Category
 {
     public Category(string id, string name)
     {
-        this.Id = id;
-        this.Name = name;
+        Id = id;
+        Name = name;
     }
 
     public string Id { get; set; }
--- Ingredients/Startup.cs
+++ Ingredients/Startup.cs
@@ -4,6 +4,7 @@ using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.OpenApi.Models;
 using Neo4j.Driver;
 
 namespace Ingredients;
@@ -22,13 +23,18 @@ public class Startup
         var neo4JSettings = new Neo4JOptions();
         Configuration.GetSection("Neo4JOptions").Bind(neo4JSettings);
 
-        services.AddControllers();
-        services.AddSingleton<IDriver>(_ => GraphDatabase
-            .Driver(neo4JSettings.Neo4JConnection, AuthTokens.Basic(neo4JSettings.Neo4JUser, neo4JSettings.Neo4JPassword)));
-        
-        services.AddScoped<INeo4JDataAccess, Neo4JDataAccess>();
+        services.AddSwaggerGen(c =>
+        {
+            c.EnableAnnotations();
+            c.SwaggerDoc("v1", new OpenApiInfo { Title = "Fennec API", Version = "v1" });
 
-        services.AddTransient<IIngredientsRepository, IngredientsRepository>();
+            var filePath = Path.Combine(AppContext.BaseDirectory, "Ingredients.xml");
+            c.IncludeXmlComments(filePath);
+        });
+        
+        services.AddControllers();
+        services.AddSingleton<IDriver>(_ => GraphDatabase.Driver(neo4JSettings.Neo4JConnection, AuthTokens.None));
+        services.AddSingleton<IIngredientsRepository, IngredientsRepository>();
     }
 
     public void ConfigureHost(ConfigureHostBuilder host, IConfiguration configuration)
@@ -38,6 +44,13 @@ public class Startup
     public Task Configure(WebApplication app, IWebHostEnvironment env)
     {
         app.UsePathBase("/api");
+        
+        app.UseSwagger();
+        app.UseSwaggerUI(c =>
+        {
+            c.SwaggerEndpoint("/api/swagger/v1/swagger.json", "Fennec API V1");
+            c.RoutePrefix = "swagger";
+        });
         
         app.MapControllers();
         return Task.CompletedTask;
