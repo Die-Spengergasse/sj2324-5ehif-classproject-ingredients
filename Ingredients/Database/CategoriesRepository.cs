dead6706fd998c05812482b85c6aa8838664ae25
necoder27 <95191364+necoder27@users.noreply.github.com>
2024-01-15 13:54:39 +0100

    feat: add category controller and repository skeleton

---
+++ Ingredients/Database/CategoriesRepository.cs
@@ -0,0 +1,56 @@
+﻿using Neo4j.Driver;
+using Category = Ingredients.Model.Category;
+
+namespace Ingredients.Database;
+
+public interface ICategoriesRepository
+{
+    public Task<string> CreateCategory(Category category);
+    
+    public Task<Category?> GetCategory(string id);
+    
+    public Task<IEnumerable<Category>> GetCategoriesWithMatchingName(string name);
+    
+    public Task UpdateCategory(string id, Category category);
+    
+    public Task<Category?> DeleteCategory(string id);
+}
+
+public class CategoriesRepository : ICategoriesRepository
+{
+    private readonly IDriver _driver;
+
+    public CategoriesRepository(IDriver driver)
+    {
+        _driver = driver;
+    }
+
+    public async Task<string> CreateCategory(Category category)
+    {
+        throw new NotImplementedException();
+
+    }
+    
+    public async Task<Category?> GetCategory(string id)
+    {
+        throw new NotImplementedException();
+
+    }
+    
+    public async Task<IEnumerable<Category>> GetCategoriesWithMatchingName(string name)
+    {
+        throw new NotImplementedException();
+
+    }
+    
+    public async Task UpdateCategory(string id, Category category)
+    {
+        throw new NotImplementedException();
+    }
+    
+    public async Task<Category?> DeleteCategory(string id)
+    {
+        throw new NotImplementedException();
+
+    }
+}
